1523c6359d50574d631fbd7a6317812e28c5613f
Alherd <algerd.blr@gmail.com>
2020-12-12 01:42:03 +0200

    Графики

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,65 @@
-for (let id of ["1293" ,"1218" ,"1253" ,"1166", "36"]){
-    fetch(process.env.VUE_APP_API_URL + '/judge/' + id ).then(
+import Vue from 'vue'
+
+for (let id of ["1293", "1218", "1253", "1166", "36"]) {
+    fetch(process.env.VUE_APP_API_URL + '/judge/' + id).then(
         r => r.json()
     ).then(
         r => {
             document.getElementById(id + '_fines_rub').innerText = r.statistic.fines_rub;
-            document.getElementById(id + '_arrests').innerText = r.statistic.arrests;
+            document.getElementById(id + '_arrests').innerText   = r.statistic.arrests;
         }
     )
 }
+
+import regions from './../../data/regions.json';
+
+
+new Vue(
+    {
+        el     : '#app',
+        methods: {
+            calculateHeight(year, region) {
+                if (year === this.maxYear && this.maxRegion === region) {
+                    return '200px'
+                }
+                let value = Math.round(this.total[year][region] / this.total[this.maxYear][this.maxRegion] * 200)
+                return (value === 0 ? 1 : value) + 'px';
+            }
+        },
+        data() {
+            return {
+                regions,
+                total    : null,
+                maxYear  : 2020,
+                maxRegion: '07'
+            }
+        },
+        mounted() {
+            fetch(process.env.VUE_APP_API_URL + '/statistic').then(
+                r => {
+                    if (!r.ok) {
+                        return null;
+                    }
+                    return r.json()
+                }
+            ).then(r => {
+                if (!r) {
+                    return;
+                }
+                this.total     = r.data.regions;
+                this.maxRegion = r.data.max.region;
+                this.maxYear   = r.data.max.year;
+                document
+                    .getElementById('total_fines_rub')
+                    .innerText = parseInt(r.data.total.finesRub).toLocaleString().replace(/,/g, ' ') + ' р.';
+                document
+                    .getElementById('total_fines')
+                    .innerText = parseInt(r.data.total.fines).toLocaleString().replace(/,/g, ' ') + ' базовых величин';
+
+                document
+                    .getElementById('total_arrest')
+                    .innerText = parseInt(r.data.total.arrests).toLocaleString().replace(/,/g, ' ');
+            })
+        }
+    }
+);
